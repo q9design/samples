d7e4fbc394fac967f2fc5c46244f3910f116c55c
Adam <adam.mitchell@q9design.com>
2015-05-05 09:36:46 -0700

    Create client.js

---
+++ node/socket/client.js
@@ -0,0 +1,17 @@
+
+var net = require('net');
+
+var client = net.connect({port: 8124, host: '10.0.0.1'},
+    function() { //'connect' listener
+  console.log('connected to server!');
+  client.write('world!\r\n');
+});
+
+client.on('data', function(data) {
+  console.log(data.toString());
+  client.end();
+});
+
+client.on('end', function() {
+  console.log('disconnected from server');
+});
