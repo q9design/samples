41bd557500b228df2b8ddd509b50f66c05cf1aea
adam.mitchell <adam.mitchell@q9design.com>
2015-10-20 11:28:30 -0400

    live-server-module

---
+++ node/modules/live-server-module/progamatic.js
@@ -0,0 +1,15 @@
+var liveServer = require('live-server')
+
+var params = {
+    //port: 8181, // Set the server port. Defaults to 8080. 
+    //host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0. 
+    //root: "/public", // Set root directory that's being server. Defaults to cwd. 
+    //open: false, // When false, it won't load your browser by default. 
+    //ignore: 'scss,my/templates', // comma-separated string for paths to ignore 
+    file: "index.html", // When set, serve this file for every 404 (useful for single-page applications) 
+    wait: 3500 // Waits for all changes, before reloading. Defaults to 0 sec. 
+};
+
+liveServer.start(params);
+
+console.log('continuing on!') // doesn't reach here.
