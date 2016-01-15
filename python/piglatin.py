f1789155e35a8f29f19ac60f8ff41a86bc6934fa
crutchcorn <crutchcorn@gmail.com>
2016-01-14 22:06:40 -0800

    Improved piglatin to include mutipule words

--- python/piglatin.py
+++ python/piglatin.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3.5
-NonPigWord=input("Enter a word to be turned into Pig Latin")
-print(NonPigWord[1:]+NonPigWord[0]+"ay")
-# Python slices work as the following: variable[start:end]
+NonPigString=input("Enter a word to be turned into Pig Latin")
+NonPigArray=NonPigString.split()
+for i in range(len(NonPigArray)): 
+    print(NonPigArray[i][1:]+NonPigArray[i][0]+"ay", end=" ")
+    # Python slices work as the following: variable[start:end]
