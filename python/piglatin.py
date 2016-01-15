0629233e3f20ebec0f0ea50add1d81c1fb39b0a6
crutchcorn <crutchcorn@gmail.com>
2016-01-14 18:54:09 -0800

    Added Python slice explaination

--- python/piglatin.py
+++ python/piglatin.py
@@ -1,3 +1,4 @@
 #!/usr/bin/env python3.5
 NonPigWord=input("Enter a word to be turned into Pig Latin")
 print(NonPigWord[1:]+NonPigWord[0]+"ay")
+# Python slices work as the following: variable[start:end]
