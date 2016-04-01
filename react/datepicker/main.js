3cf933c0e2d5b94923e990c77736091c7e8744c1
adam.mitchell <adam.mitchell@q9design.com>
2016-04-01 14:53:09 -0400

    webpack

---
+++ react/datepicker/main.js
@@ -0,0 +1,35 @@
+var React = require('react')
+var DatePicker = require('react-datepicker')
+var moment = require('moment')
+var ReactDom = require('react-dom')
+
+
+//require('react-datepicker/dist/react-datepicker.css');
+ 
+var Example = React.createClass({
+  displayName: 'Example',
+ 
+  getInitialState: function() {
+    return {
+      startDate: moment()
+    };
+  },
+ 
+  handleChange: function(date) {
+    this.setState({
+      startDate: date
+    });
+  },
+ 
+  render: function() {
+    return <DatePicker
+        selected={this.state.startDate}
+        onChange={this.handleChange} />;
+  }
+});
+
+
+ReactDom.render(
+	<DatePicker />,
+	document.querySelector('#app')
+	)
---
+++ webpack/includecss/entry.js
@@ -0,0 +1,3 @@
+require("./style.css")
+document.write('it works!')
+document.write(require('./content.js'))
