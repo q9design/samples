a93fbedc43cb4826a96764e7e28c2df21e63b0fd
adam.mitchell <adam.mitchell@q9design.com>
2016-06-10 13:44:01 -0400

    u

--- node/udp/detect-mtu/mtu-client.js
+++ node/udp/detect-mtu/mtu-client.js
@@ -1,13 +1,20 @@
 //
-// basic client (transmitter)
+// basic client (transmitter) -- test packetsize sendable
 //
+
+
+// next: tool to detect MTU via avoid fragmanatation
+
+//
+// data can be sent up to about 65507 (MAX - (20+9)) but might be fragmented at network packet level.  Try to keep as close to fit in a single frame MTU.
+// 
 
 const dgram = require('dgram')
 const client  = dgram.createSocket('udp4')
 
 
 var i = 1;
-var buffer = Buffer.alloc(65507) // 65507 on local host 
+var buffer = Buffer.alloc(65507) // 65507 on local host // 25004 on wireless? .. 1472 bytes recomended? (to avoid udp fragmentation)
 
 var max = buffer.byteLength
 
