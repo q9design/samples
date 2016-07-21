2a6bf5a52adcf0e1bdde073526ee33a4407bac6a
adam.mitchell <adam.mitchell@q9design.com>
2016-07-20 22:03:51 -0400

    u

--- service/twilio/nts/basic.js
+++ service/twilio/nts/basic.js
@@ -24,25 +24,25 @@ request({
 
 /*
 
- '{"username": "831a4a2921959e2c117b8ce21b589ec4ee20e7a07e669cc2dd13f147d5a7b8d9", "password": "y4kMWsz2PXR924UtYHRNIfm8z461EEzSKncZa12tBgw=", "account_sid": "ACa9889691caa924c7bd3416d13de2903a", "ttl": "86400", "ice_servers": [{"url": "stun:global.stun.twilio.com:3478?transport=udp"}, {"url": "turn:global.turn.twilio.com:3478?transport=udp", "username": "831a4a2921959e2c117b8ce21b589ec4ee20e7a07e669cc2dd13f147d5a7b8d9", "credential": "y4kMWsz2PXR924UtYHRNIfm8z461EEzSKncZa12tBgw="}, {"url": "turn:global.turn.twilio.com:3478?transport=tcp", "username": "831a4a2921959e2c117b8ce21b589ec4ee20e7a07e669cc2dd13f147d5a7b8d9", "credential": "y4kMWsz2PXR924UtYHRNIfm8z461EEzSKncZa12tBgw="}, {"url": "turn:global.turn.twilio.com:443?transport=tcp", "username": "831a4a2921959e2c117b8ce21b589ec4ee20e7a07e669cc2dd13f147d5a7b8d9", "credential": "y4kMWsz2PXR924UtYHRNIfm8z461EEzSKncZa12tBgw="}], "date_created": "Thu, 21 Jul 2016 00:34:27 +0000", "date_updated": "Thu, 21 Jul 2016 00:34:27 +0000"}'
+ '{"username": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "password": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=", "account_sid": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "ttl": "86400", "ice_servers": [{"url": "stun:global.stun.twilio.com:3478?transport=udp"}, {"url": "turn:global.turn.twilio.com:3478?transport=udp", "username": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "credential": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx="}, {"url": "turn:global.turn.twilio.com:3478?transport=tcp", "username": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "credential": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx="}, {"url": "turn:global.turn.twilio.com:443?transport=tcp", "username": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "credential": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx="}], "date_created": "Thu, 21 Jul 2016 00:34:27 +0000", "date_updated": "Thu, 21 Jul 2016 00:34:27 +0000"}'
 
 
 
-{ username: '693f5cafa874569a506b1301d631ed5e17e8dcbf04f772cd429c4e51f2ade2fb',
-  password: 'N3Pzvj601TwNe99YVuwNqW6TbBGLTtvEYocfBz8aMNU=',
-  account_sid: 'ACa9889691caa924c7bd3416d13de2903a',
+{ username: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+  password: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=',
+  account_sid: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
   ttl: '86400',
   ice_servers: 
    [ { url: 'stun:global.stun.twilio.com:3478?transport=udp' },
      { url: 'turn:global.turn.twilio.com:3478?transport=udp',
-       username: '693f5cafa874569a506b1301d631ed5e17e8dcbf04f772cd429c4e51f2ade2fb',
-       credential: 'N3Pzvj601TwNe99YVuwNqW6TbBGLTtvEYocfBz8aMNU=' },
+       username: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+       credential: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=' },
      { url: 'turn:global.turn.twilio.com:3478?transport=tcp',
-       username: '693f5cafa874569a506b1301d631ed5e17e8dcbf04f772cd429c4e51f2ade2fb',
-       credential: 'N3Pzvj601TwNe99YVuwNqW6TbBGLTtvEYocfBz8aMNU=' },
+       username: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+       credential: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=' },
      { url: 'turn:global.turn.twilio.com:443?transport=tcp',
-       username: '693f5cafa874569a506b1301d631ed5e17e8dcbf04f772cd429c4e51f2ade2fb',
-       credential: 'N3Pzvj601TwNe99YVuwNqW6TbBGLTtvEYocfBz8aMNU=' } ],
+       username: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+       credential: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=' } ],
   date_created: 'Thu, 21 Jul 2016 00:35:33 +0000',
   date_updated: 'Thu, 21 Jul 2016 00:35:33 +0000' }
 
