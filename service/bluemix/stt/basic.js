93b3b0cd16405a96fbaf911def1c4aedce906612
adam.mitchell <adam.mitchell@q9design.com>
2016-08-05 19:38:34 -0400

    u

--- service/bluemix/stt/basic.js
+++ service/bluemix/stt/basic.js
@@ -4,17 +4,15 @@ var fs = require('fs');
 var c = JSON.parse(fs.readFileSync('config.json').toString())
 console.log(c)
  
-var text_to_speech = watson.text_to_speech({
+var text_to_speech = watson.speech_to_text({
   username: c.credentials.username,
   password: c.credentials.password,
   version: 'v1'
 });
  
-var params = {
-  text: 'Hello from IBM Watson',
-  voice: 'en-US_AllisonVoice', // Optional voice 
-  accept: 'audio/wav'
-};
- 
-// Pipe the synthesized text to a file 
-text_to_speech.synthesize(params).pipe(fs.createWriteStream('output.wav'));
+
+//upg: stream from input command line pipe. .. or is there a stream 
+fs.createReadStream('./resources/speech.wav')
+  .pipe(speech_to_text.createRecognizeStream({ content_type: 'audio/l16; rate=44100' }))
+  .pipe(fs.createWriteStream('./transcription.txt'));
+
