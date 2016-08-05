59f897be7497f6e434d30d2b34ed6d53c33aa47f
adam.mitchell <adam.mitchell@q9design.com>
2016-08-05 19:28:12 -0400

    u

--- service/bluemix/tts/basic.js
+++ service/bluemix/tts/basic.js
@@ -1,1 +1,20 @@
-L:sdf
+var watson = require('watson-developer-cloud');
+var fs = require('fs');
+
+var c = fs.readFileAsync('config.json').toString()
+console.log(c)
+ 
+var text_to_speech = watson.text_to_speech({
+  username: '<username>',
+  password: '<password>',
+  version: 'v1'
+});
+ 
+var params = {
+  text: 'Hello from IBM Watson',
+  voice: 'en-US_AllisonVoice', // Optional voice 
+  accept: 'audio/wav'
+};
+ 
+// Pipe the synthesized text to a file 
+text_to_speech.synthesize(params).pipe(fs.createWriteStream('output.wav'));
